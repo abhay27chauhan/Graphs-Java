317a052e6d3e15b772757b353b4ffc28ec17bb03
Abhay <chauhan27.abhay@gmail.com>
2020-09-09 03:35:48 +0530

    get connected components (few changes)

--- getConnectedPaths.java
+++ getConnectedPaths.java
@@ -32,10 +32,21 @@ class getConnectedPaths{
         }
 
         graph.get(0).add(new Edge(0,1,10));
+
+        graph.get(1).add(new Edge(1,0,10));
+
         graph.get(2).add(new Edge(2,3,10));
+
+        graph.get(3).add(new Edge(3,2,10));
+
         graph.get(4).add(new Edge(4,5,10));
         graph.get(4).add(new Edge(4,6,10));
+
+        graph.get(5).add(new Edge(5,4,10));
         graph.get(5).add(new Edge(5,6,10));
+
+        graph.get(6).add(new Edge(6,4,10));
+        graph.get(6).add(new Edge(6,5,10));
 
         ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
         boolean[] visited = new boolean[vtces];
